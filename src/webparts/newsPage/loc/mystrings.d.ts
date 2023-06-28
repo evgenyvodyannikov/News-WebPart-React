71d51e2e52c8a3b2c8b6063960402adab5f8abb7
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-06-28 16:54:07 +0500

    init

---
+++ src/webparts/newsPage/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface INewsPageWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'NewsPageWebPartStrings' {
+  const strings: INewsPageWebPartStrings;
+  export = strings;
+}
